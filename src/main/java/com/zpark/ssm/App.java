91c3136c8021556dfa75e851bcbf0a4263d46021
male <male@163.cn>
2023-05-05 18:12:35 +0800

    male master second commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,5 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git! male master second commit");
+
     }
 }
