1a9fafee76e21919a0d4dfe06fbba2a564fe39c5
Vie12315 <100424287+Vie12315@users.noreply.github.com>
2023-05-06 17:10:40 +0800

    Update App.java

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -14,7 +14,7 @@ public class App {
         System.out.println("Test02 Pull From GitHub");
         System.out.println("Hello GitHub!male master third push GitHub!");
         System.out.println("Test03 Pull From GitHub");
-        
+        System.out.println("Clone to IDEA From GitHub");
 
 
     }
