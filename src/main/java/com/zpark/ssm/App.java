584eb5218908a017049bea11246860e7e25f7303
male <male@163.cn>
2023-05-06 09:05:58 +0800

    male hotfix first commit!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git! male master second commit");
+        System.out.println("Hello Git!male hotfix first commit!");
 
     }
 }
