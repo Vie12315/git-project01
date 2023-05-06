90797227b7a5c28de384eeb336d057320fd33a7a
male <male@163.cn>
2023-05-06 10:48:11 +0800

    male master first push GitHub!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -8,6 +8,7 @@ public class App {
         System.out.println("Hello Git!male hotfix first commit!");
         System.out.println("Hello Git!male hotfix first modify!");
         System.out.println("Hello Git!male master first modify!");
+        System.out.println("Hello GitHub!male master first push GitHub!");
 
     }
 }
